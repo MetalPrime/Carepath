7af6bf8738989caa40b167496434896e22291e23
Jhon Garcia <metalprime57@gmail.com>
2020-12-07 15:26:01 -0500

    update custom_button.xml

--- app/src/main/java/com/example/carepath/MainActivity.java
+++ app/src/main/java/com/example/carepath/MainActivity.java
@@ -3,12 +3,16 @@ package com.example.carepath;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    View view;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
